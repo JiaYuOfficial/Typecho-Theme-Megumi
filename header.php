446cebb7cdc7f482ca6665b934f24f42d8d140d5
JiaYuOfficial <hotococoa@yeah.net>
2022-07-12 23:36:09 +0800

    适配1.2，预备夜间样式

--- header.php
+++ header.php
@@ -1,62 +1,65 @@
-<?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title><?php $this -> archiveTitle(array(
+	
+	
+	<head>
+		<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/style.css">
+		<link rel="stylesheet" href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/bootstrap/bootstrap-icons.css">
+		 <script src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/megumi.js"></script>
+		<meta http-equiv="content-type" content="text/html; charset=<?php $this->options->charset(); ?>" />
+		<?php $this->header(); ?>
+		<link rel="icon" href="<?php $this -> options -> logoUrl(); ?>" sizes="192x192"/>
+	    <title><?php $this -> archiveTitle(array(
             'category'  =>  _t('%s'),
             'search'    =>  _t('含关键词 %s 的文章'),
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
         ), '', ' - '); $this -> options -> title(); ?></title>
-<?php if($this -> options -> favicon): ?>
-    <link rel="icon" href="<?php $this -> options -> favicon(); ?>" sizes="192x192"/>
-<?php else: ?>
-    <link rel="icon" href="<?php $this -> options -> themeUrl('img/icon.png'); ?>" sizes="192x192"/>
-<?php endif; ?>
-    <link href="<?php $this -> options -> themeUrl('static/kico.css'); ?>" rel="stylesheet" type="text/css"/>
-    <link href="<?php $this -> options -> themeUrl('static/single.css'); ?>" rel="stylesheet" type="text/css"/>
-    <link href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1"/>
-<?php if($this -> options -> background): ?>
-    <style>body:before{content: ''; background-image: url(<?php $this -> options -> background(); ?>)}</style>
-<?php endif; ?>
-<?php if($this -> options -> custom_css): ?>
-    <style><?php $this -> options -> custom_css(); ?></style>
-<?php endif; ?>
-    <meta property="og:site_name" content="<?php $this -> options -> title(); ?>">
-    <meta property="og:title" content="<?php $this -> archiveTitle(array(
-            'category' => _t('%s'),
-            'search'   => _t('含关键词 %s 的文章'),
-            'tag'      => _t('标签 %s 下的文章'),
-            'author'   => _t('%s 发布的文章')
-        ), ""); ?>"/>
-<?php $this -> header('generator=&template=&pingback=&xmlrpc=&wlw='); ?>
-</head>
-<body<?php Single::is_night() ?>>
-<header>
-    <div class="head-title">
-        <h4><?php $this -> options -> title(); ?></h4>
-    </div>
-    <div class="head-action">
-        <div class="toggle-btn"></div>
-        <div class="light-btn"></div>
-        <div class="search-btn"></div>
-    </div>
-    <form class="head-search" method="post">
-        <input type="text" name="s" placeholder="搜索什么？">
-    </form>
-    <nav class="head-menu">
-        <a href="<?php $this -> options -> siteUrl(); ?>">首页</a>
-        <div class="has-child">
-            <a>分类</a>
-            <div class="sub-menu">
-                <?php $this -> widget('Widget_Metas_Category_List') -> parse('<a href="{permalink}">{name}</a>'); ?>
-            </div>
-        </div>
-        <?php $this -> widget('Widget_Contents_Page_List') -> parse('<a href="{permalink}">{title}</a>'); ?>
-<?php if($this -> user -> hasLogin()): ?>
-        <a href="<?php $this -> options -> adminUrl(); ?>" target="_blank">进入后台</a>
-<?php endif; ?>
-    </nav>
-</header>
+	    <style type="text/css"><?php $this -> options -> custom_css(); ?></style>
+
+        
+        <link rel="stylesheet"href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/prism.css">
+        <script src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/prism.js"></script>
+	   
+	
+	    
+	    		 <!--灯箱-->
+		 <script type="text/javascript">$(document).ready(function () {$( ".fancybox").fancybox();});</script>
+        <script type="text/javascript" src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/jquery.min.js"></script> 
+        <!--如果主题已经引用了jQuery库，可以忽略这条-->
+        <link rel="stylesheet" href="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/fancybox/jquery.fancybox.min.css">
+        <script src="<?php $this->options->siteUrl(); ?>usr/themes/Megumi/other/fancybox/jquery.fancybox.min.js"></script>
+
+        <style>
+        body:before{opacity: 0.1;}
+        </style>
+
+        <?php if($this -> options -> background): ?>
+        <style>body:before{background-image: url(<?php $this -> options -> background(); ?>)}
+        body:before{opacity: <?php $this -> options -> background_other(); ?>;}
+        </style>
+        <?php endif; ?>
+	
+	</head>
+	
+	<header>
+			<nav class="index-nav">
+				<div class="index-nav-tittle"><?php $this->options->title() ?></div>
+				<div  class="index-nav-links">
+					<a href="<?php $this->options->siteUrl(); ?>">主页</a>
+			
+					<a class="nav-second-menu">分类</a>
+					<div class="nav-menu1">
+					    <?php $this -> widget('Widget_Metas_Category_List') -> parse('<a href="{permalink}">{name}</a>'); ?>
+					</div>
+				
+					
+					<?php $this->widget('Widget_Contents_Page_List')
+					   ->parse('<a href="{permalink}">{title}</a>'); ?>
+					
+					<!--<?php if($this -> user -> hasLogin()): ?>-->
+                        <a href="<?php $this -> options -> adminUrl(); ?>" target="_blank">后台</a>
+                    <!--<?php endif; ?>-->
+				</div>
+			</nav>
+	</header>
